5c5cb9d795a8de93605ab37e7d0fd494029a71b1
George <George@192.168.0.10>
2017-09-21 01:26:14 -0400

    initial commit

---
+++ public_html/JavaScript/header.js
@@ -0,0 +1,120 @@
+(function () {
+
+    var width, height, largeHeader, dayTime, nightTime, canvas, ctx, circles,
+    target,scrolltop, animateHeader = true;
+
+    // Main
+    initHeader();
+    addListeners();
+
+    function initHeader() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        target = {x: 0, y: height};
+
+        nightTime = document.getElementById('day-time');
+        nightTime.style.height = height + 'px';
+
+        dayTime = document.getElementById('night-time');
+        dayTime.style.height = height + 'px';
+            
+        dayTime.style.opacity = "0";
+        largeHeader = document.getElementById('large-header');
+        largeHeader.style.height = height + 'px';
+
+        canvas = document.getElementById('demo-canvas');
+        canvas.width = width;
+        canvas.height = height;
+        ctx = canvas.getContext('2d');
+
+        // create particles
+        circles = [];
+        for (var x = 0; x < width * 0.5; x++) {
+            var c = new Circle();
+            circles.push(c);
+        }
+        animate();
+    }
+
+    // Event handling
+    function addListeners() {
+        window.addEventListener('scroll', scrollCheck);
+        window.addEventListener('resize', resize);
+    }
+
+    function scrollCheck() {
+        
+        scrolltop = document.body.scrollTop;
+        dayTime.style.opacity = (1 + scrolltop/700-1);
+      //  document.getElementById("kicker").innerHTML = (1 + (scrolltop/1000)-1 );
+        if (document.body.scrollTop > height)
+            animateHeader = false;
+        else
+            animateHeader = true;
+    }
+
+    function resize() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        largeHeader.style.height = height + 'px';
+        nightTime.style.height = height + 'px';
+        dayTime.style.height = height + 'px';
+        canvas.width = width;
+        canvas.height = height;
+    }
+
+    function animate() {
+        if (animateHeader) {
+            ctx.clearRect(0, 0, width, height);
+            for (var i in circles) {
+                circles[i].draw();
+            }
+        }
+        requestAnimationFrame(animate);
+    }
+
+    // Canvas manipulation
+    function Circle() {
+        var _this = this;
+
+        // constructor
+        (function () {
+            _this.pos = {};
+            init();
+            console.log(_this);
+        })();
+
+        function init() {
+            _this.pos.x = Math.random() * width;
+            _this.pos.y = height + Math.random() * 100;
+            _this.alpha = 0.1 + Math.random() * 0.4;
+            _this.scale = 0.1 + Math.random() * 0.4;
+            _this.velocity = Math.random();
+        }
+
+        this.draw = function () {
+            if (_this.alpha <= 0) {
+                init();
+            }
+            _this.pos.y -= _this.velocity;
+            _this.alpha -= 0.0005;
+            ctx.beginPath();
+            ctx.arc(_this.pos.x, _this.pos.y, _this.scale * 10, 0, 2 * Math.PI, false);
+            ctx.fillStyle = 'rgba(255,255,255,' + _this.alpha + ')';
+            ctx.fill();
+        };
+    }
+
+})();
+
+
+
+
+
+/*var header = $("#header");
+ 
+ $(window).on("scroll", function() {
+ var scrolltop = $(this).scrollTop();
+ header.css({"opacity": (1 - scrolltop/880)});
+ // $('#pTest').text(scrolltop);
+ });*/
